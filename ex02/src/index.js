fcfa61edf9625b08c7ce131963ebf767e75f81e4
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2021-09-15 10:41:11 +0200

    added index.js file for comments

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+  //Only change code belowe this line
+  //This is a single line JavaScript comment
+  /* document.write("this is a multi-line comment!");
+  document.write("this is a multi-line comment!");
+  document.write("this is a multi-line comment!");
+  document.write("this is a multi-line comment!");
+  document.write("this is a multi-line comment!");
+*/
+  //Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
