1a132d344bbf322bd6be6a94b1484afab41c0704
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-21 21:56:19 +0200

    made first go build

--- app.go
+++ app.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"test/helper"
 )
 
 func main(){
 	helper.Help()
-	data := Greet("adnan")
+	data, err := Greet("")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(data)
 }
--- greet.go
+++ greet.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func Greet(name string) string{
+func Greet(name string) (string, error){
+	if name == ""{
+		return "",errors.New("missing name param")
+	}
 	hy := fmt.Sprintf("heeloooo %v.", name)
-	return hy
+	return hy,nil
 	
 }
 
